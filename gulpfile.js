474a60e176f8979f86f07dac483dc12453c5077e
MabinKim <mabin0910@gmail.com>
2018-04-26 23:58:49 +0900

    [원격수업] tables 호작질

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,26 @@ var gulp = require('gulp');
 
 // gulp plugin 호출
 var concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-  sourcemaps = require('gulp-sourcemaps'),
-  scss = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    scss = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    fileInclude = require('gulp-file-include');
 
+ 
 var src = 'resources/src';
 var dist = 'resources';
 var paths = {
-  commonjs : src + '/js/*.js',/*
-  funcjs : src + '/js/!*.js',*/
+  commonjs : src + '/js/*.js',
   scss : src + '/scss/common-ui.scss',
-  scssAll : src + '/scss/*.scss'
+  scssAll : src + '/scss/*.scss',
+  includeHtml : src + '/lottopang/html/*.html'
 };
 
+
+
+/* JS TASK(S) */
 gulp.task('js_common_concat', function() {
   return gulp.src(paths.commonjs)
     .pipe(concat('common-ui.js'))
@@ -25,9 +30,11 @@ gulp.task('js_common_concat', function() {
     .pipe(rename('common-ui.min.js'))
     .pipe(gulp.dest(dist + '/js'))
 });
+/* JS TASK(E) */
 
+/* SASS TASK(S) */
 var scssOptions = {
-  outputStyle: 'compact', //values : nested, expanded, compact, compressed
+  outputStyle: 'expanded', //values : nested, expanded, compact, compressed
   indentType: 'space',
   indentWidth: 4, //outpusStyle이 nested, expanded인 경우 사용
   precision: 6 //컴파일된 css 소수점 자리수
@@ -40,23 +47,29 @@ gulp.task('scss_compile', function() {
       .pipe(sourcemaps.write('sourcemaps')) 
       .pipe(gulp.dest(dist + '/css'))
 });
+/* SASS TASK(E) */
+
+
+/* HTML INCLUDE TASK(S) */
+gulp.task('html_include',function(){
+    return gulp.src(paths.includeHtml)
+      .pipe(fileInclude({
+        prefix: '@@',
+        basepath: './resources/src/include'
+      }))
+    .pipe(gulp.dest(dist + '/html'));
+
+});
+/* HTML INCLUDE TASK(E) */
+
+
 
 gulp.task('watch', function() {
   gulp.watch(paths.commonjs, ['js_common_concat']);
   gulp.watch(paths.funcjs, ['js_func_concat']);
   gulp.watch(paths.scssAll, ['scss_compile']);
+  gulp.watch(paths.includeHtml, ['html_include']);
 });
 
-gulp.task('default', ['js_common_concat', /*'js_func_concat', */'scss_compile', 'watch']);
+gulp.task('default', ['js_common_concat','scss_compile','html_include', 'watch']);
 
-var fileinclude = require('gulp-file-include'),
-  gulp = require('gulp');
- 
-gulp.task('fileinclude', function() {
-  gulp.src(['index.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./'));
-});
